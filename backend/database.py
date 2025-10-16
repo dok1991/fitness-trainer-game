c48b0c8762701763ecdaaa4bba55610f60d24432
dok1991 <dok1991bk@gmail.com>
2025-10-16 16:58:50 +0500

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -1,12 +1,11 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL").replace("postgresql://", "postgresql+pg8000://")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
